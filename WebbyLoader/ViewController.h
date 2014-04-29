16506bd51201a898853b7b303e358ea7dbfe3a62
Robert Figueras <robertfigueras@Roberts-iMac-4.local>
2014-04-29 10:39:08 -0500

    Initial Commit

---
+++ WebbyLoader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WebbyLoader
+//
+//  Created by Robert Figueras on 4/29/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WebbyLoaderTests/WebbyLoaderTests.m
@@ -0,0 +1,34 @@
+//
+//  WebbyLoaderTests.m
+//  WebbyLoaderTests
+//
+//  Created by Robert Figueras on 4/29/14.
+//  Copyright (c) 2014 AppSpaceship. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebbyLoaderTests : XCTestCase
+
+@end
+
+@implementation WebbyLoaderTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
